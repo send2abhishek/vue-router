34c886c386ca05e864b85d9eab8061dd92628d88
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-10-24 10:34:15 +0530

    add routers

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UserLists from './components/users/UsersList.vue';
+
+const app = createApp(App);
+
+const router = createRouter({
+  routes: [
+    {
+      path: '/teams',
+      component: TeamsList
+    },
+    {
+      path: '/users',
+      component: UserLists
+    }
+  ],
+  history: createWebHistory()
+});
+app.use(router);
+app.mount('#app');
